dffcafb2cb184d9a7829ec54b55a34a17456079e
amanita4t <amanueltekalign4@gmail.com>
2025-08-14 23:13:26 +0300

    twitter sentiment analysis first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import tweepy
+from textblob import TextBlob
+import pandas as pd
+
+#Twitter API authentication
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAJ1C3gEAAAAAUh6Yc1OxDdROxRqyXlpDvAD2Qv8%3DJNgHaFohwJ0fHry2LEYbK8fpsixKqjAsBGcMn3acynPxwydkZq"
+client = tweepy.Client(bearer_token=bearer_token)
+
+#Search query
+query = "Trump -is:retweet lang:en"
+
+#Fetch tweets
+tweets = client.search_recent_tweets(query=query, max_results=90)
+
+#Process tweets
+data = []
+for tweet in tweets.data:
+    analysis = TextBlob(tweet.text)
+    data.append({
+        "Tweet": tweet.text,
+        "Polarity": analysis.sentiment.polarity,
+        "Subjectivity": analysis.sentiment.subjectivity
+    })
+    
+#Save to CSV 
+df = pd.DataFrame(data)
+df.index += 1  # start index from 1
+df.to_csv("tweets.csv", index_label="Index")
